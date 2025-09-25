473f5c8223cd854fdb976bf52ad110789f0f4153
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-24 20:32:24 -0700

    done ex04.js

--- ex04.js
+++ ex04.js
@@ -1,3 +1,12 @@
 // Includes Exercise
 // Instructions: Create an array of at least five different fruits as strings. Write code to check if the array includes the fruit "apple".
 
+const fruits = ["banana", "apple", "orange", "peach", "grape", "melon"];
+
+const includedApple = fruits.includes("apple");
+
+if (includedApple) {
+  console.log("apple is included");
+} else {
+  console.log("apple is not here");
+}
