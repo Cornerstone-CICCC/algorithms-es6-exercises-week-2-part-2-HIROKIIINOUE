9758cdc0404ce8718e54e9507186f0ebddd7a49c
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-24 20:29:56 -0700

    done ex03.js

--- ex03.js
+++ ex03.js
@@ -1,3 +1,8 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+const numbers = [88, 41, 1, 24, 99, 14];
+
+const reversedNumbers = numbers.reverse();
+
+console.log(reversedNumbers);
