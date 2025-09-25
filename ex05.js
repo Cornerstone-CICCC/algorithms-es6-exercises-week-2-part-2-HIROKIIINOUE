444e836d92ddf28b4d83f16f0f8083ac47cf58ae
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-24 20:35:04 -0700

    done ex05.js

--- ex05.js
+++ ex05.js
@@ -1,3 +1,12 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const numbers = [45, 7, 11, 67, 9, 41, 401];
+
+const includedEvenNumber = numbers.some((number) => number % 2 === 0);
+
+if (includedEvenNumber) {
+  console.log("even number is included");
+} else {
+  console.log("only odd number is here");
+}
